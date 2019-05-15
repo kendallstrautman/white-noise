8cb80910bdc9427a9e6999ff8d6781441f67fea2
Kendall Strautman <kendallstraut@gmail.com>
2019-05-15 13:56:04 -0700

    additional noise functionality - play/pause all

--- src/components/Noise.js
+++ src/components/Noise.js
@@ -1,39 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Noise = props => {
-  const [noise, setNoise] = useState();
-  const handleNoise = props => {
-    if (props.color == "brown") {
-      setNoise(new Audio(require("../assets/brown.mp3")));
-    } else if (props.color == "pink") {
-      setNoise(new Audio(require("../assets/pink.mp3")));
-    } else if (props.color == "white") {
-      setNoise(new Audio(require("../assets/white-noise.mp3")));
-    } else if (props.color == "blue") {
-      setNoise(new Audio(require("../assets/blue.mp3")));
-    } else if (props.color == "violet") {
-      setNoise(new Audio(require("../assets/purple.mp3")));
-    } else if (props.color == "grey") {
-      setNoise(new Audio(require("../assets/gray.mp3")));
-    }
-  };
-
-  const [playing, setPlaying] = useState(true);
-  const toggle = () => {
-    if (playing) {
-      noise.play();
-    } else {
-      noise.pause();
-    }
-    setPlaying(!playing);
-  };
-  useEffect(() => {
-    handleNoise(props);
-  }, []);
-
   return (
-    <div className="noise" onClick={toggle}>
-      <h1 className={props.color}>{props.color}</h1>
+    <div className="noise">
+      <h1 id={props.color} className={props.color}>
+        {props.color}
+      </h1>
     </div>
   );
 };
--- src/components/RootApp.js
+++ src/components/RootApp.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Noise from "./Noise";
 import Footer from "./Footer";
 
 const RootApp = () => {
+  const [noises] = useState({
+    brown: new Audio(require("../assets/brown.mp3")),
+    pink: new Audio(require("../assets/pink.mp3")),
+    white: new Audio(require("../assets/white-noise.mp3")),
+    blue: new Audio(require("../assets/blue.mp3")),
+    violet: new Audio(require("../assets/purple.mp3")),
+    grey: new Audio(require("../assets/gray.mp3"))
+  });
+  const [playing, setPlaying] = useState(true);
+  const [colorPlaying, setColorPlaying] = useState(null);
+  const toggle = e => {
+    const i = Object.keys(noises).indexOf(e.target.id);
+    const noisePlaying = Object.values(noises)[i];
+    const color = Object.keys(noises)[i];
+    setColorPlaying(color);
+    Object.values(noises).map(noise => {
+      noise !== noisePlaying && noise.pause();
+    });
+    if (color !== colorPlaying) {
+      noisePlaying.play();
+      setPlaying(false);
+    } else {
+      if (playing) {
+        setPlaying(false);
+        noisePlaying.play();
+      } else {
+        setPlaying(true);
+        noisePlaying.pause();
+      }
+    }
+  };
+  const createNoises = () => {
+    return (
+      <section className="noises">
+        {Object.keys(noises).map(noise => {
+          return (
+            <div onClick={toggle}>
+              <Noise color={noise} />
+            </div>
+          );
+        })}
+      </section>
+    );
+  };
   return (
     <main className="main">
-      <section className="noises">
-        <Noise color="brown" src="../assets/brown.mp3" />
-        <Noise color="pink" src="../assets/pink.mp3" />
-        <Noise color="white" src="../assets/white.mp3" />
-        <Noise color="blue" src="../assets/blue.mp3" />
-        <Noise color="violet" src="../assets/purple.mp3" />
-        <Noise color="grey" src="../assets/gray.mp3" />
-      </section>
+      {createNoises()}
       <Footer />
     </main>
   );
