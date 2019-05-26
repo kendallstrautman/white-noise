cc7b48b8fba2c40048b33dbf1f73e1710bb43679
Kendall Strautman <kendallstraut@gmail.com>
2019-05-26 13:56:50 -0700

    toggling between visible components

--- src/components/RootApp.js
+++ src/components/RootApp.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NoiseMenu from "./NoiseMenu";
 import Timer from "./Timer";
+import Info from "./Info";
 import Footer from "./Footer";
 
 const RootApp = () => {
   const [timerLength, setTimerLength] = useState(0);
-  useEffect(() => {
-    console.log(timerLength);
-  });
+  const [isVisible, setIsVisible] = useState("menu");
   return (
     <main className="main">
       <NoiseMenu timerLength={timerLength} />
       <Timer timerLength={timerLength} setTimerLength={setTimerLength} />
-      <Footer />
+      <Info />
+      <Footer isVisible={isVisible} setIsVisible={setIsVisible} />
     </main>
   );
 };
--- src/components/TimerContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const TimerContext = React.createContext({
-  time: "0"
-});
-
-export const Provider = TimerContext.Provider;
-export const Consumer = TimerContext.Consumer;
