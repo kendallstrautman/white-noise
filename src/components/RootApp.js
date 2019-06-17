f726b732e1d3a189a91521d292c941d4a6b84a9b
Kendall Strautman <kendallstraut@gmail.com>
2019-06-17 12:33:31 -0700

    added dynamic height calculation for mobile

--- src/components/RootApp.js
+++ src/components/RootApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NoiseMenu from "./NoiseMenu/NoiseMenu";
 import Timer from "./Timer/Timer";
 import Info from "./Info/Info";
@@ -7,8 +7,12 @@ import Footer from "./Footer/Footer";
 const RootApp = () => {
   const [timerLength, setTimerLength] = useState(0);
   const [visibleTab, setVisibleTab] = useState("menu");
+  const [deviceHeight, setDeviceHeight] = useState(null);
+  useEffect(function calcDeviceHeight() {
+    setDeviceHeight(window.innerHeight);
+  });
   return (
-    <main className="main">
+    <main className="main" style={{ height: deviceHeight && deviceHeight }}>
       <NoiseMenu
         timerLength={timerLength}
         setTimerLength={setTimerLength}
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -8,20 +8,29 @@ const Timer = props => {
   }
 
   return (
-    <div
-      className={`timer  ${
+    <section
+      className={`timer ${
         props.visibleTab == "timer" ? "--isActive" : "--isHidden"
       }`}
     >
-      <label>
-        Time in Minutes <br />
-        <input
-          type="number"
-          value={props.timerLength}
-          onChange={handleSetTimer}
-        />
-      </label>
-    </div>
+      <div className="timer--wrap">
+        <svg
+          className="outer-circle"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle className="outer-circle__color" cx="50" cy="50" r="57" />
+          <circle className="outer-circle__base" cx="50" cy="50" r="57" />
+          <circle
+            draggable="true"
+            className="draggable-circle"
+            cx="50"
+            cy="-6"
+            r="6"
+          />
+        </svg>
+      </div>
+    </section>
   );
 };
 
@@ -32,3 +41,38 @@ Timer.propTypes = {
 };
 
 export default Timer;
+
+// import React from "react";
+// import PropTypes from "prop-types";
+
+// const Timer = props => {
+//   function handleSetTimer(e) {
+//     const time = e.target.value;
+//     props.setTimerLength(time);
+//   }
+
+//   return (
+//     <div
+//       className={`timer  ${
+//         props.visibleTab == "timer" ? "--isActive" : "--isHidden"
+//       }`}
+//     >
+//       <label>
+//         Time in Minutes <br />
+//         <input
+//           type="number"
+//           value={props.timerLength}
+//           onChange={handleSetTimer}
+//         />
+//       </label>
+//     </div>
+//   );
+// };
+
+// Timer.propTypes = {
+//   timerLength: PropTypes.number.isRequired,
+//   setTimerLength: PropTypes.func.isRequired,
+//   visibleTab: PropTypes.string.isRequired
+// };
+
+// export default Timer;
